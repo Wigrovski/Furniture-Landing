671c29cd2136f7bcd127b26a9ff4d264ff68ef5d
Wigrovski <wigrovski@mail.ru>
2022-03-24 01:27:53 +0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,13 @@
+$('.best_slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.nav_slider'
+});
+$('.nav_slider').slick({
+    infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    asNavFor: '.best_slider'
+});
